22ac9b2f3877109c80fc0bd27561ccc900027532
DeviousWings <nksteele@byui.edu>
2023-07-25 12:53:47 -0600

    creating api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/user");
 
-app.post("/register", (req, res) => {
-  res.json("test ok2");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://nskingdev:<OVGhKDUp1mlZnHvb>@cluster0.zrtybye.mongodb.net/"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.create({
+    username,
+    password,
+  });
+  res.json(userDoc);
 });
 
 app.listen(4000);
+//nskingdev
+//OVGhKDUp1mlZnHvb
--- frontend/src/components/auth/register.js
+++ frontend/src/components/auth/register.js
@@ -1,17 +1,41 @@
-import register from "../images/register.jpg";
+import { useState } from "react";
+
+import registerPic from "../images/register.jpg";
+// import { json } from "stream/consumers";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
     <div>
-      <form className='register'>
+      <form className='register' onSubmit={register}>
         <h1>Register</h1>
-        <input type='text' placeholder='username' />
-        <input type='password' placeholder='password' />
+        <input
+          type='text'
+          placeholder='username'
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
         <button>Register</button>
       </form>
       <div className='registerImg'>
         <a href='https://www.artstation.com/artwork/space-outpost-bce36731-badd-4f50-a73b-cb40696b5306'>
-          <img src={register} alt='Space Outpost' />
+          <img src={registerPic} alt='Space Outpost' />
           <h6>by Tim Witprächtiger</h6>
         </a>
       </div>
